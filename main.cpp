f69eebdc9d7c534cf883c63a40e8a9ff308fc4cc
mario <mobrown4@asu.edu>
2026-09-11 11:59:20 -0700

    Final commit2

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ int main(){
 //get input from user
     //number of elements
 int elementCount;
-cout << "Enter how many elements in the array: ";
+//cout << "Enter how many elements in the array: ";
 cin >> elementCount;
 cout << endl;
 int numArr[elementCount] = {0};
@@ -28,18 +28,18 @@ int numArr[elementCount] = {0};
 
 
 for(int i = 0; i < elementCount; i++){
-    cout << "enter a number in the array: ";
+    //cout << "enter a number in the array: ";
     cin >> numArr[i];
     
 }
 
-cout << "Your entered: ";
+/*cout << "Your entered: ";
 for(int i = 0; i < elementCount; i++){
     cout << numArr[i] << " ";
 }
-cout << endl;
+cout << endl;*/
 bubbleSort(numArr, elementCount);
-cout << "The sorted array is: ";
+//cout << "The sorted array is: ";
 for(int i = 0; i < elementCount; i++){
     cout << numArr[i] << " ";
 }
